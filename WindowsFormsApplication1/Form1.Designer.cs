a70355e971bd7bedc5eecccee62d1a86707f6e19
gorquan <gorquanwu@gmail.com>
2016-12-17 23:37:26 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(371, 504);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 43);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "后退\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox3.Location = new System.Drawing.Point(646, 113);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(448, 117);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(118, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 43);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox4.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox4.Location = new System.Drawing.Point(222, 345);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(82, 106);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(118, 345);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(98, 106);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox2.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox2.Location = new System.Drawing.Point(366, 345);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(152, 106);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(367, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 41);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "座位号";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(118, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 41);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "组号";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "表情包抱枕~",
+            "纪念版卡片U盘~",
+            "守望先锋盒子抱枕~"});
+            this.comboBox1.Location = new System.Drawing.Point(194, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(291, 33);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1159, 660);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NA5.0映画抽奖系统 Power By 糯米糍";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int count = 0, alpan = 0;
+        public bool buttonStart = false, judge = true;
+        public string select;
+        //创建字符串数组防止重复抽取
+        public string[] already = new string[18];
+        public int nums = 0;
+        public string output;
+        /// <summary>
+        /// 随机数程序，产生字母随机数和座位号随机数
+        /// </summary>
+        private void randomNumberProgram()
+        {
+
+            try
+            {
+                Random r = new Random();
+                //为字母随机数新建线程
+                Thread th1 = new Thread(randomAlpanResult);
+                //后台放置于后台
+                th1.IsBackground = true;
+                //线程开始
+                th1.Start();
+                int resultTextbox2Number = 0, resulTextbox4Number = 0;
+                while (buttonStart == true)
+                {
+                    resultTextbox2Number = r.Next(1, 13);
+                    textBox2.Text = resultTextbox2Number.ToString("D2");
+                    //tostring后面加Dx（x为数字）则可以输出多少位数
+                    if (resultTextbox2Number > 6)
+                    {
+                        textBox4.Text = "2";
+                        randomAlpanResult();
+                    }
+                    else
+                    {
+                        resulTextbox4Number = r.Next(1, 3);
+                        //判断大组号
+                        switch (resulTextbox4Number)
+                        {
+                            case 1:
+                                textBox4.Text = "1";
+                                break;
+                            case 2:
+                                textBox4.Text = "3";
+                                break;
+                        }
+                        randomAlpanResult();
+                    }
+                }
+            }
+            catch { }
+        }
+        /// <summary>
+        /// 抽奖事件触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //触发事件，开始抽奖
+                if (button1.Text == "开始")
+                {
+                    //如果抽奖次数不等于0
+                    if (count != 0)
+                    {
+                        buttonStart = true;
+                        button1.Text = "停止";
+                        //为随机数程序创建线程
+                        Thread th = new Thread(randomNumberProgram);
+                        //线程处于后台
+                        th.IsBackground = true;
+                        //线程开始
+                        th.Start();
+                    }
+                    //如果抽奖次数等于0且没有选择项目
+                    else if (count == 0 && select == null)
+                    {
+                        MessageBox.Show("你还没有选择抽奖项目哦~");
+                    }
+                    //其他情况均为抽奖次数用尽
+                    else
+                    {
+                        MessageBox.Show("当前抽奖次数用尽了哦~");
+                    }
+                }
+                else if (button1.Text == "停止")
+                {
+                    buttonStart = false;
+                    //抽奖次数减一
+                    count--;
+                    button1.Text = "开始";
+                    //输出剩余信息
+                    textbox3Output();
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //去掉线程检查
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 每个奖品抽奖次数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                select = comboBox1.SelectedItem.ToString();
+                if (select == "守望先锋盒子抱枕~" && count == 0)
+                {
+                    count = 1;
+                    textBox3.Text = select;
+                }
+                else if ((select == "纪念版卡片U盘~" || select == "表情包抱枕~") && count == 0)
+                {
+                    count = 3;
+                    textBox3.Text = select;
+                }
+            }
+            catch
+            { }
+        }
+
+        /// <summary>
+        /// 出现意外时候增加抽奖次数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //次数大于三或者奖项只有一个的守望先锋抱枕或者不选择抽奖项的时候不增加
+                if (count == 3 || select == "守望先锋盒子抱枕~" && count == 1 || (select == null && count == 0))
+                { }
+                else
+                {
+                    //增加抽奖次数
+                    count += 1;
+                    textBox3.Clear();
+                    textBox3.Text = (select + "抽奖机会还有" + count.ToString() + "次");
+                }
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 随机字母函数
+        /// </summary>
+        private void randomAlpanResult()
+        {
+            try
+            {
+                Random rd = new Random();
+                int resultAlpanNumber = 0;
+                resultAlpanNumber = rd.Next(1, 16);
+                switch (resultAlpanNumber)
+                {
+                    case 1:
+                        textBox1.Text = "A";
+                        break;
+                    case 2:
+                        textBox1.Text = "B";
+                        break;
+                    case 3:
+                        textBox1.Text = "C";
+                        break;
+                    case 4:
+                        textBox1.Text = "D";
+                        break;
+                    case 5:
+                        textBox1.Text = "E";
+                        break;
+                    case 6:
+                        textBox1.Text = "F";
+                        break;
+                    case 7:
+                        textBox1.Text = "G";
+                        break;
+                    case 8:
+                        textBox1.Text = "H";
+                        break;
+                    case 9:
+                        textBox1.Text = "I";
+                        break;
+                    case 10:
+                        textBox1.Text = "J";
+                        break;
+                    case 11:
+                        textBox1.Text = "K";
+                        break;
+                    case 12:
+                        textBox1.Text = "L";
+                        break;
+                    case 13:
+                        textBox1.Text = "M";
+                        break;
+                    case 14:
+                        textBox1.Text = "N";
+                        break;
+                    case 15:
+                        textBox1.Text = "O";
+                        break;
+                }
+            }
+            catch { }
+        }
+        /// <summary>
+        /// 信息文本框输出程序
+        /// </summary>
+        private void textbox3Output()
+        {
+            try
+            {
+                output = textBox1.Text + textBox4.Text + textBox2.Text;
+                for (int i = 0; i < nums; i++)
+                {
+                    if (output == already[i])
+                    {
+                        judge = false;
+                    }
+                }
+                if (judge == true)
+                {
+                    textBox3.Clear();
+                    already[nums] = output;
+                    nums += 1;
+                    if (count != 0)
+                    {
+                        textBox3.Text = (select + "抽奖机会还有" + count.ToString() + "次");
+                    }
+                    else if (count == 0)
+                    {
+                        textBox3.AppendText("当前没有抽奖机会了~");
+                    }
+                }
+                else if (judge == false)
+                {
+                    MessageBox.Show("该号码中过奖，请重新抽取");
+                    count += 1;
+                }
+            }
+            catch { }
+        }
+    }
+
+}
